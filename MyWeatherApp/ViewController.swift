cd962f329a4ddb975b738dff6f57a7747991c2bc
J-Eisen <jeisenstock@gmail.com>
2019-01-07 12:52:23 -0500

    Initial Commit

---
+++ MyWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWeatherApp
+//
+//  Created by Jonah Eisenstock on 1/7/19.
+//  Copyright © 2019 JonahEisenstock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
